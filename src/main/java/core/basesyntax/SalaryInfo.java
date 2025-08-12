dd9b0c8c4005a74a08b65a708a8a21a6af9208a5
kck_game <golembjovskijm@gmail.com>
2025-08-12 23:26:39 +0200

    removed hashList, added ordinary int[] solution

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -3,16 +3,17 @@ package core.basesyntax;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 
 public class SalaryInfo {
-    private final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private static final DateTimeFormatter dateTimeFormatter =
+            DateTimeFormatter.ofPattern("dd.MM.yyyy");
 
-    public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
+    public String getSalaryInfo(String[] names, String[] data,
+                                String dateFrom, String dateTo) {
         LocalDate from = LocalDate.parse(dateFrom, dateTimeFormatter);
         LocalDate to = LocalDate.parse(dateTo, dateTimeFormatter);
-        Map<String, Integer> salaryMap = new HashMap<>();
+
+        int[] salaries = new int[names.length];
 
         for (String record : data) {
             String[] parts = record.split(" ");
@@ -33,17 +34,20 @@ public class SalaryInfo {
             int rate = Integer.parseInt(parts[3]);
             int salary = hours * rate;
 
-            salaryMap.put(name, salaryMap.getOrDefault(name, 0) + salary);
-
+            int index = Arrays.asList(names).indexOf(name);
+            salaries[index] += salary;
         }
 
         StringBuilder builder = new StringBuilder();
         builder.append("Report for period ").append(dateFrom).append(" - ").append(dateTo)
                 .append(System.lineSeparator());
 
-        for (String name : names) {
-            int totalSalary = salaryMap.getOrDefault(name, 0);
-            builder.append(name).append(" - ").append(totalSalary).append(System.lineSeparator());
+        for (int i = 0; i < names.length; i++) {
+            int totalSalary = salaries[i];
+
+            builder.append(names[i]).append(" - ")
+                    .append(totalSalary)
+                    .append(System.lineSeparator());
         }
 
         return builder.toString().trim();
